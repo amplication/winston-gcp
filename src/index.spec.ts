315a19a387bcd7feffb841f5eb18bc9e0944bcf3
Iddan Aaronsohn <mail@aniddan.com>
2020-09-27 17:05:08 +0300

    Fix exported API

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,6 +1,4 @@
-import * as winston from "winston";
-import { LEVEL, MESSAGE } from "triple-beam";
-import { format, createLogger, WinstonLevel, CloudLoggingSeverity } from ".";
+import { format, WinstonLevel, CloudLoggingSeverity } from ".";
 
 const EXAMPLE_MESSAGE = "Example message";
 
@@ -10,34 +8,5 @@ describe("format", () => {
     expect(format().transform(info)).toBe(info);
     // @ts-ignore
     expect(info.severity).toBe(CloudLoggingSeverity.Error);
-  });
-});
-
-describe("createLogger", () => {
-  test("creates logger", () => {
-    const logMock = jest.fn((info, callback) => {
-      callback();
-    });
-    const transport = new winston.transports.Console();
-    // @ts-ignore
-    transport.log = logMock;
-    const logger = createLogger({
-      transports: [transport],
-    });
-    logger.info(EXAMPLE_MESSAGE);
-    expect(logMock).toBeCalledWith(
-      {
-        message: EXAMPLE_MESSAGE,
-        level: WinstonLevel.Info,
-        severity: CloudLoggingSeverity.Info,
-        [LEVEL]: WinstonLevel.Info,
-        [MESSAGE]: JSON.stringify({
-          message: EXAMPLE_MESSAGE,
-          level: WinstonLevel.Info,
-          severity: CloudLoggingSeverity.Info,
-        }),
-      },
-      expect.any(Function)
-    );
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,9 @@ export enum CloudLoggingSeverity {
   Debug = "DEBUG",
 }
 
-export const LEVEL_TO_SEVERITY: Record<string, string> = {
+export const LEVEL_TO_SEVERITY: {
+  [level in WinstonLevel]: CloudLoggingSeverity;
+} = {
   [WinstonLevel.Emergency]: CloudLoggingSeverity.Emergency,
   [WinstonLevel.Alert]: CloudLoggingSeverity.Alert,
   [WinstonLevel.Critical]: CloudLoggingSeverity.Critical,
@@ -37,25 +39,9 @@ export const LEVEL_TO_SEVERITY: Record<string, string> = {
  * Creates Cloud Logging Winston format.
  * The format transformation maps sWinston "level" log property to the Cloud Logging compatible "severity" log property.
  */
-export const format = (): winston.Logform.Format => {
-  return winston.format((info) => {
-    info.severity = LEVEL_TO_SEVERITY[info.level];
-    return info;
-  })();
-};
-
-/**
- * Creates a new Winston logger with JSON and Cloud Logging formats.
- * If transports are not provided defaults to Console transport.
- * @returns Winston logger
- */
-export function createLogger(options?: winston.LoggerOptions): winston.Logger {
-  const formats = [format(), winston.format.json()];
-  if (options?.format) {
-    formats.unshift(options?.format);
+export const format = winston.format((info) => {
+  if (info.level in LEVEL_TO_SEVERITY) {
+    info.severity = LEVEL_TO_SEVERITY[info.level as WinstonLevel];
   }
-  return winston.createLogger({
-    ...options,
-    format: winston.format.combine(...formats),
-  });
-}
+  return info;
+});
